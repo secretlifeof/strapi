460b47c1f6eac2811fe0b0df9c3939757f7c6ff2
Espen Finnesand <38684618+secretlifeof@users.noreply.github.com>
2020-02-18 14:04:54 +0100

    Update Inputs
    Corrected formatting on relational Field. There was a double formatting className="col-6" on the parent and this child.

--- packages/strapi-plugin-content-manager/admin/src/components/Inputs/index.js
+++ packages/strapi-plugin-content-manager/admin/src/components/Inputs/index.js
@@ -93,7 +93,7 @@ function Inputs({ autoFocus, keys, layout, name, onBlur }) {
 
   if (type === 'relation') {
     return (
-      <div className="col-6" key={keys}>
+      <div className="col-12" key={keys}>
         <SelectWrapper
           {...metadatas}
           name={keys}
